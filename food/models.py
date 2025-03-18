98e4e0177a4706dd046477740acfe3f85c732cdb
dabananda <imdmitra@gmail.com>
2025-03-18 17:06:25 +0600

    Static files & Site design

--- food/models.py
+++ food/models.py
@@ -9,3 +9,5 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500, default='https://cdn-icons-png.flaticon.com/512/1377/1377194.png')
