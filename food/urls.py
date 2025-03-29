8c78cff2ce6a52ac61498b73da765d7c33419b75
dabananda <imdmitra@gmail.com>
2025-03-29 20:11:34 +0600

    Implemented Generic ListView & DetailView

--- food/urls.py
+++ food/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from food import views
 
 app_name = 'food'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('', views.IndexClassView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailClassView.as_view(), name='detail'),
     path('create/', views.create_item, name='create_item'),
     path('update/<int:item_id>', views.update_item, name='update_item'),
     path('delete/<int:item_id>', views.delete_item, name='delete_item'),
--- food/views.py
+++ food/views.py
@@ -1,22 +1,19 @@
 from django.shortcuts import render, redirect
 from food.models import Item
 from food.forms import ItemForm
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def index(request):
-    item_list = Item.objects.all()
-    context = {
-        'item_list': item_list,
-    }
-    return render(request, 'food/index.html', context)
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item': item,
-    }
-    return render(request, 'food/detail.html', context)
+class DetailClassView(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 
 
 def create_item(request):
