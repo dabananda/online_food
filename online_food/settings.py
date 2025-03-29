652de3f55d833f6e6e1998ad90a4fb7a7c2be81f
dabananda <imdmitra@gmail.com>
2025-03-29 20:42:19 +0600

    Database setup

--- online_food/settings.py
+++ online_food/settings.py
@@ -61,13 +61,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'online_food.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_online_food',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
